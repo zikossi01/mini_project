691b7d2d84b921d47bc542e7fba4aa5d3f090e5c
zakariamoumni9@gmail.com <zakariamoumni9@gmail.com>
2025-09-22 14:59:02 +0100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-500">
+        Tailwind is working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",                // main HTML file
+    "./src/**/*.{js,jsx,ts,tsx}"   // all JS/TS/React files in src and subfolders
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
